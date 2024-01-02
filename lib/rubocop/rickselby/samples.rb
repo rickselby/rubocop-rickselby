d9af395761023a61aac2d3f2414c2ae874282630
Rick Selby <rick@selby-family.co.uk>
2024-01-02 15:23:13 +0000

    Only include samples that differ from the rubocop defaults

--- lib/rubocop/rickselby/samples.rb
+++ lib/rubocop/rickselby/samples.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 module Rubocop
-  # Sample code to see if rubocop does the right thing
+  # Sample code to see how rubocop styles the samples
   module Rickselby
-    def begin_end_block
-      a = begin
-        foo = bar
-        foo + baz
-      end
-      a + 1
-    end
-
     def block_alignment
       foo.bar.baz do
         sth
@@ -20,14 +12,6 @@ module Rubocop
          .baz do
            sth
          end
-    end
-
-    def case_statement_assignment
-      a = case foo
-          when 1 then bar
-          else baz
-          end
-      a + 1
     end
   end
 end
