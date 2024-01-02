29f96d1fead8f3442b19fc6d2b82bc51c888b030
Rick Selby <rick@selby-family.co.uk>
2024-01-02 15:56:17 +0000

    Add hash and array layout options

---
+++ lib/rubocop/rickselby/layout.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+module Rubocop
+  module RickSelby
+    # Sample code to see how rubocop styles the samples
+    module Layout
+      def block_alignment
+        foo.bar.baz do
+          sth
+        end
+
+        foo.bar
+           .baz do
+             sth
+           end
+      end
+
+      def first_array_element_line_break
+        [
+          1,
+          2
+        ]
+      end
+
+      def first_hash_element_line_break
+        {
+          a: 1,
+          b: 2
+        }
+      end
+
+      def hash_alignment
+        a = {
+          a:  1,
+          bc: 2
+        }
+
+        b = {
+          1  => 1,
+          23 => 2
+        }
+
+        a + b
+      end
+
+      def multiline_array_line_breaks
+        [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6
+        ]
+      end
+
+      def multiline_assignment_layout
+        foo = if expression
+                2
+              else
+                3
+              end
+        foo + 1
+      end
+
+      def multiline_hash_key_line_breaks
+        {
+          a: 1,
+          b: 2,
+          c: 3
+        }
+      end
+    end
+  end
+end
--- rubocop-rickselby.gemspec
+++ rubocop-rickselby.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |spec|
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir["README.md", "STYLEGUIDE.md", "LICENSE.text", "config/*.yml", "lib/**/*.rb"]
+  spec.files = Dir["README.md", "STYLEGUIDE.md", "LICENSE.text", "config/*.yml", "lib/rubocop/rickselby/version.rb"]
 
   spec.add_dependency "rubocop", "~> 1.59.0"
   spec.add_dependency "rubocop-performance", "~> 1.20.0"
